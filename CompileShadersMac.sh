ada3ffdc9880a26849d5a049ef25c523804424a9
Levi <155780172+levispev@users.noreply.github.com>
2025-03-06 15:21:47 -0800

    Proper error handling for Mac shader compilation

--- CompileShadersMac.sh
+++ CompileShadersMac.sh
@@ -8,6 +8,13 @@ fi
 mkdir -p ./bin/shaders
 
 "$VULKAN_SDK/bin/glslc" ./Tempus/res/shaders/shader.vert -o ./bin/shaders/vert.spv
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 "$VULKAN_SDK/bin/glslc" ./Tempus/res/shaders/shader.frag -o ./bin/shaders/frag.spv
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 echo "Successfully compiled shaders."
